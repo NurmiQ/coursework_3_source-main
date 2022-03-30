67c68e3c82a680dc0da1bf0e183e1924f899d8d2
NurmiQ <v-kogan@rambler.ru>
2022-03-30 21:15:13 +0300

    models/movie correction

--- project/dao/models/movie.py
+++ project/dao/models/movie.py
@@ -4,16 +4,15 @@ from project.setup_db import db
 
 class Movie(BaseMixin, db.Model):
     __tablename__ = 'movies'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    trailer = db.Column(db.String(255))
-    year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("genres")
-    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    director = db.relationship("directors")
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    trailer = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=False)
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey('directors.id'), nullable=False)
+    director = db.relationship("Director")
 
     def __repr__(self):
         return f"<Movie '{self.name.title()}'>"
